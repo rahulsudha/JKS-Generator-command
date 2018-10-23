36d6f30bc0f84727ba6b8a7c32ad309887a1be23
Rahul Sudha <rahulsudha143@gmail.com>
2018-10-24 01:36:39 +0530

    added copy function
    added copy function

--- keystoregen.js
+++ keystoregen.js
@@ -68,22 +68,43 @@ function parse()
 
 
 var dname = "CN="+document.getElementById('servername').value+", OU=Web Portal Services, O=Barclays PLC, L=London, ST=London, C=GB"
-var line01 = "keytool -genkey -noprompt \\ ";
+var line01 = "keytool -genkey -noprompt \\";
 var line02 = "-alias "+document.getElementById('aliasname').value+" \\";
 var line03 = "-dname "+"\""+dname+"\""+" \\";
 var line04 = "-keystore "+document.getElementById('keystorename').value+" \\";
 var line05 = "-storepass "+document.getElementById('keystorepass').value+" \\";
 var line06 = "-keypass "+document.getElementById('aliaspass').value;
 
-alert(line01);
-alert(line02);
-alert(line03);
-alert(line04);
-alert(line05);
-alert(line06);
+// alert(line01);
+// alert(line02);
+// alert(line03);
+// alert(line04);
+// alert(line05);
+// alert(line06);
+
+document.getElementById('textareaout').value = line01+"\n"+line02+"\n"+line03+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
+
+var line01a = "keytool -certreq -noprompt \\"
+
+document.getElementById('textareaoutcsr').value = line01a+"\n"+line02+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
+
+
+
+}
 
 
 
 
+
+function jkscopy()
+{
+ $("#textareaout").select();
+    document.execCommand('copy'); 
+}
+
+function csrcopy()
+{
+  $("#textareaoutcsr").select();
+    document.execCommand('copy');
 }
 
