8a794188da5f14f47a883026d67134aedbcd0580
Rahul Sudha <rahulsudha143@gmail.com>
2018-10-24 09:36:42 +0530

    Added validation function
    Added validation function

--- keystoregen.js
+++ keystoregen.js
@@ -2,7 +2,6 @@
 
 $(document).ready(function()
 {
-
   function equalizepassword()
   {
     var keystorepassword = document.getElementById('keystorepass').value;
@@ -11,20 +10,21 @@ $(document).ready(function()
     //alert(document.getElementById('aliaspass').value);
   }
 
-  $("#checkpass").change(function () {
+  $("#checkpass").change(function () 
+  {
     if($(this).prop("checked"))
     {
       $("#aliaspass").attr("disabled", "disabled");
       equalizepassword();
     }
-    else {
+    else 
+    {
       $("#aliaspass").removeAttr("disabled", "disabled");
     }
-    });
+  });
 
 
     $(function(){
-
       $("input[type=radio]").change(function(){
           //alert( $(this).val() );
           var radiovalue = $(this).val();
@@ -50,50 +50,91 @@ $(document).ready(function()
             document.getElementById('keystorepass').value = "";
           }
       });
-
     });
-
 
 // main function ends below
 });
 
 function validateform()
 {
-  
+  if (document.getElementById('servername').value == "") 
+  { 
+    $("#servername_class").addClass("error");
+
+    $("#aliasname_class").removeClass("error");
+    $("#keystorename_class").removeClass("error");
+    $("#keystorepass_class").removeClass("error");
+    $("#aliaspass_class").removeClass("error");
+  }
+  else if (document.getElementById('aliasname').value == "") 
+  {
+    $("#aliasname_class").addClass("error");
+
+    $("#servername_class").removeClass("error");
+    $("#keystorename_class").removeClass("error");
+    $("#keystorepass_class").removeClass("error");
+    $("#aliaspass_class").removeClass("error");    
+  }
+  else if (document.getElementById('keystorename').value == "") 
+  {
+    $("#keystorename_class").addClass("error");
+
+    $("#servername_class").removeClass("error");
+    $("#aliasname_class").removeClass("error");
+    $("#keystorepass_class").removeClass("error");
+    $("#aliaspass_class").removeClass("error");    
+  }
+  else if (document.getElementById('keystorepass').value == "") 
+  {
+    $("#keystorepass_class").addClass("error");
+
+    $("#servername_class").removeClass("error");
+    $("#aliasname_class").removeClass("error");
+    $("#keystorename_class").removeClass("error");
+    $("#aliaspass_class").removeClass("error");
+  }
+  else if (document.getElementById('aliaspass').value == "") 
+  {
+    $("#aliaspass_class").addClass("error");
+
+    $("#servername_class").removeClass("error");
+    $("#aliasname_class").removeClass("error");
+    $("#keystorename_class").removeClass("error");
+    $("#keystorepass_class").removeClass("error");
+  }
+  else
+  {
+    $("#servername_class").removeClass("error");
+    $("#aliasname_class").removeClass("error");
+    $("#keystorename_class").removeClass("error");
+    $("#keystorepass_class").removeClass("error");
+    $("#aliaspass_class").removeClass("error");
+    parse();
+  }
 }
 
 
 function parse()
 {
+  var dname = "CN="+document.getElementById('servername').value+", OU=Web Portal Services, O=Barclays PLC, L=London, ST=London, C=GB"
+  var line01 = "keytool -genkey -noprompt \\";
+  var line02 = "-alias "+document.getElementById('aliasname').value+" \\";
+  var line03 = "-dname "+"\""+dname+"\""+" \\";
+  var line04 = "-keystore "+document.getElementById('keystorename').value+" \\";
+  var line05 = "-storepass "+document.getElementById('keystorepass').value+" \\";
+  var line06 = "-keypass "+document.getElementById('aliaspass').value;
+  var line01a = "keytool -certreq -noprompt \\"
 
+  // alert(line01);
+  // alert(line02);
+  // alert(line03);
+  // alert(line04);
+  // alert(line05);
+  // alert(line06);
 
-var dname = "CN="+document.getElementById('servername').value+", OU=Web Portal Services, O=Barclays PLC, L=London, ST=London, C=GB"
-var line01 = "keytool -genkey -noprompt \\";
-var line02 = "-alias "+document.getElementById('aliasname').value+" \\";
-var line03 = "-dname "+"\""+dname+"\""+" \\";
-var line04 = "-keystore "+document.getElementById('keystorename').value+" \\";
-var line05 = "-storepass "+document.getElementById('keystorepass').value+" \\";
-var line06 = "-keypass "+document.getElementById('aliaspass').value;
-
-// alert(line01);
-// alert(line02);
-// alert(line03);
-// alert(line04);
-// alert(line05);
-// alert(line06);
-
-document.getElementById('textareaout').value = line01+"\n"+line02+"\n"+line03+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
-
-var line01a = "keytool -certreq -noprompt \\"
-
-document.getElementById('textareaoutcsr').value = line01a+"\n"+line02+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
-
-
-
+  document.getElementById('textareaout').value = line01+"\n"+line02+"\n"+line03+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
+  document.getElementById('textareaoutcsr').value = line01a+"\n"+line02+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
 }
-
-
-
 
 
 function jkscopy()
