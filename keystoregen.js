663b024cd4398b779706db6919fcf0108937bb99
Rahul Sudha <rahulsudha143@gmail.com>
2018-11-16 23:55:24 +0530

    Added more fields and help

--- keystoregen.js
+++ keystoregen.js
@@ -1,151 +1,231 @@
 
-
-$(document).ready(function()
-{
-  function equalizepassword()
-  {
-    var keystorepassword = document.getElementById('keystorepass').value;
-    //alert(keystorepassword);
-    document.getElementById('aliaspass').value = keystorepassword;
-    //alert(document.getElementById('aliaspass').value);
-  }
-
-  $("#checkpass").change(function () 
-  {
-    if($(this).prop("checked"))
-    {
-      $("#aliaspass").attr("disabled", "disabled");
-      equalizepassword();
-    }
-    else 
-    {
-      $("#aliaspass").removeAttr("disabled", "disabled");
-    }
-  });
+//+---------------------------+//
+//| Script written by Rahul S |//
+//+---------------------------+//
+//| rahulsudha143@gmail.com   |//
+//+---------------------------+//
 
 
-    $(function(){
-      $("input[type=radio]").change(function(){
-          //alert( $(this).val() );
-          var radiovalue = $(this).val();
-          //alert(radiovalue);
-          if (radiovalue == "my")
-          {
-            //alert("my selected \\");
-            document.getElementById('aliasname').value = "mykey";
-            document.getElementById('keystorename').value = "mystore.jks";
-            document.getElementById('keystorepass').value = "barcap.com";
-          }
-          else if (radiovalue == "prod")
-          {
-            //alert("prod selected");
-            document.getElementById('aliasname').value = "mykey";
-            document.getElementById('keystorename').value = "prodstore.jks";
-            document.getElementById('keystorepass').value = "portal";
-          }
-          else
-          {
-            document.getElementById('aliasname').value = "";
-            document.getElementById('keystorename').value = "";
-            document.getElementById('keystorepass').value = "";
-          }
-      });
+$(function(){
+    $("#helpbutton").click(function(){
+        $(".helpmodal").modal('show');
     });
-
-// main function ends below
+    $(".helpmodal").modal({
+        closable: true
+    });
 });
 
-function validateform()
+
+
+$(document).ready(function() {
+
+  //Function to keep the Alias and Keystore password the same
+
+  function equalizepassword() {
+        var keystorepassword = document.getElementById('keystorepass').value;
+        document.getElementById('aliaspass').value = keystorepassword;
+    }
+
+// Below checks for the checkbox to see if above functions needs to be called or not.
+
+    $("#checkpass").change(function() {
+        if ($(this).prop("checked")) {
+
+            if (document.getElementById('keystorepass').value == "") {
+                alert("Keystore passowrd is empty!")
+                document.getElementById("checkpass").checked = false;
+            } else {
+                $("#aliaspass").attr("disabled", "disabled");
+                equalizepassword();
+            }
+        } else {
+            $("#aliaspass").removeAttr("disabled", "disabled");
+            document.getElementById('aliaspass').value = "";
+        }
+    });
+
+
+}); // document on load function ends here
+
+//Validation function starts below
+function removeerrorclass()
 {
-  if (document.getElementById('servername').value == "") 
-  { 
-    $("#servername_class").addClass("error");
-
-    $("#aliasname_class").removeClass("error");
-    $("#keystorename_class").removeClass("error");
-    $("#keystorepass_class").removeClass("error");
-    $("#aliaspass_class").removeClass("error");
-  }
-  else if (document.getElementById('aliasname').value == "") 
-  {
-    $("#aliasname_class").addClass("error");
-
-    $("#servername_class").removeClass("error");
-    $("#keystorename_class").removeClass("error");
-    $("#keystorepass_class").removeClass("error");
-    $("#aliaspass_class").removeClass("error");    
-  }
-  else if (document.getElementById('keystorename').value == "") 
-  {
-    $("#keystorename_class").addClass("error");
-
-    $("#servername_class").removeClass("error");
-    $("#aliasname_class").removeClass("error");
-    $("#keystorepass_class").removeClass("error");
-    $("#aliaspass_class").removeClass("error");    
-  }
-  else if (document.getElementById('keystorepass').value == "") 
-  {
-    $("#keystorepass_class").addClass("error");
-
-    $("#servername_class").removeClass("error");
-    $("#aliasname_class").removeClass("error");
-    $("#keystorename_class").removeClass("error");
-    $("#aliaspass_class").removeClass("error");
-  }
-  else if (document.getElementById('aliaspass').value == "") 
-  {
-    $("#aliaspass_class").addClass("error");
-
-    $("#servername_class").removeClass("error");
-    $("#aliasname_class").removeClass("error");
-    $("#keystorename_class").removeClass("error");
-    $("#keystorepass_class").removeClass("error");
-  }
-  else
-  {
     $("#servername_class").removeClass("error");
     $("#aliasname_class").removeClass("error");
     $("#keystorename_class").removeClass("error");
     $("#keystorepass_class").removeClass("error");
     $("#aliaspass_class").removeClass("error");
-    parse();
-  }
+    $("#OU_class").removeClass("error");
+    $("#Org_class").removeClass("error");
+    $("#locality_class").removeClass("error");
+    $("#state_class").removeClass("error");
 }
 
 
-function parse()
+function validateform() {
+
+    document.getElementById('textareaout').value = "";
+    document.getElementById('textareaoutcsr').value = "";
+
+    if (document.getElementById('servername').value == "") {
+        $("#servername_class").addClass("error");
+
+        $("#aliasname_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#keystorepass_class").removeClass("error");
+        $("#aliaspass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#Org_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('aliasname').value == "") {
+        $("#aliasname_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#keystorepass_class").removeClass("error");
+        $("#aliaspass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#Org_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('keystorename').value == "") {
+        $("#keystorename_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#aliasname_class").removeClass("error");
+        $("#keystorepass_class").removeClass("error");
+        $("#aliaspass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#Org_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('keystorepass').value == "") {
+        $("#keystorepass_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#aliasname_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#aliaspass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#Org_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('aliaspass').value == "") {
+        $("#aliaspass_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#aliasname_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#keystorepass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#Org_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('OUname').value == "") {
+        $("#OU_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#aliasname_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#keystorepass_class").removeClass("error");
+
+        $("#Org_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('Orgname').value == "") {
+        $("#Org_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#aliasname_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#keystorepass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('locality').value == "") {
+        $("#locality_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#aliasname_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#keystorepass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#Org_class").removeClass("error");
+        $("#state_class").removeClass("error");
+    } else if (document.getElementById('statename').value == "" || document.getElementById('ccname').value == "") {
+        $("#state_class").addClass("error");
+
+        $("#servername_class").removeClass("error");
+        $("#aliasname_class").removeClass("error");
+        $("#keystorename_class").removeClass("error");
+        $("#aliaspass_class").removeClass("error");
+        $("#OU_class").removeClass("error");
+        $("#Org_class").removeClass("error");
+        $("#locality_class").removeClass("error");
+
+    } else {
+        removeerrorclass();
+        parse();
+    }
+}
+
+// Function for parsing the values to form the code
+
+function parse() {
+    
+    var ou_name = document.getElementById('OUname').value;
+    var org_name = document.getElementById('Orgname').value;
+    var locality_name = document.getElementById('locality').value;
+    var state_name = document.getElementById('statename').value;
+    var cc_name = document.getElementById('ccname').value;
+
+    var dname = "CN=" + document.getElementById('servername').value + ", OU=" + ou_name +", O=" + org_name + ", L=" + locality_name + ", ST=" + state_name + ", C=" + cc_name;
+    
+    var line01 = "keytool -genkey -noprompt \\";
+    var line011 = "-keyalg RSA \\"
+    var line02 = "-alias " + document.getElementById('aliasname').value + " \\";
+    var line03 = "-dname " + "\"" + dname + "\"" + " \\";
+    var line04 = "-keystore " + document.getElementById('keystorename').value + " \\";
+    var line05 = "-storepass " + document.getElementById('keystorepass').value + " \\";
+    var line06 = "-keypass " + document.getElementById('aliaspass').value;
+    var line01a = "keytool -certreq -noprompt \\"
+
+    // Outputing the values for the JKS and CSR code
+
+    document.getElementById('textareaout').value = line01 + "\n" + line011 + "\n" + line02 + "\n" + line03 + "\n" + line04 + "\n" + line05 + "\n" + line06 + "\n";
+    document.getElementById('textareaoutcsr').value = line01a + "\n" + line02 + "\n" + line04 + "\n" + line05 + "\n" + line06 + "\n";
+}
+
+// clear button function
+
+function clearall()
 {
-  var dname = "CN="+document.getElementById('servername').value+", OU=Web Portal Services, O=Barclays PLC, L=London, ST=London, C=GB"
-  var line01 = "keytool -genkey -noprompt \\";
-  var line02 = "-alias "+document.getElementById('aliasname').value+" \\";
-  var line03 = "-dname "+"\""+dname+"\""+" \\";
-  var line04 = "-keystore "+document.getElementById('keystorename').value+" \\";
-  var line05 = "-storepass "+document.getElementById('keystorepass').value+" \\";
-  var line06 = "-keypass "+document.getElementById('aliaspass').value;
-  var line01a = "keytool -certreq -noprompt \\"
-
-  // alert(line01);
-  // alert(line02);
-  // alert(line03);
-  // alert(line04);
-  // alert(line05);
-  // alert(line06);
-
-  document.getElementById('textareaout').value = line01+"\n"+line02+"\n"+line03+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
-  document.getElementById('textareaoutcsr').value = line01a+"\n"+line02+"\n"+line04+"\n"+line05+"\n"+line06+"\n";
+    document.getElementById('textareaout').value = "";
+    document.getElementById('textareaoutcsr').value = "";
+    document.getElementById('servername').value = "";
+    document.getElementById('aliasname').value = "";
+    document.getElementById('keystorename').value ="";
+    document.getElementById('keystorepass').value ="";
+    document.getElementById('aliaspass').value ="";
+    document.getElementById('OUname').value ="";
+    document.getElementById('Orgname').value ="";
+    document.getElementById('locality').value ="";
+    document.getElementById('statename').value ="";
+    document.getElementById('ccname').value ="";
+    document.getElementById("checkpass").checked = false;
 }
 
 
-function jkscopy()
-{
- $("#textareaout").select();
-    document.execCommand('copy'); 
+// Copy functions for both JKS and CSR
+
+function jkscopy() {
+    $("#textareaout").select();
+    document.execCommand('copy');
 }
 
-function csrcopy()
-{
-  $("#textareaoutcsr").select();
+function csrcopy() {
+    $("#textareaoutcsr").select();
     document.execCommand('copy');
 }
 
